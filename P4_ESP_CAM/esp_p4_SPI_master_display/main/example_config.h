af08c858967d6f8650b3096377ef1f1bf645d0d5
Nik <nikzeman37@gmail.com>
2025-12-13 20:27:20 +0100

    uz streamuju obraz na display, ale ma tonejaky sum

---
+++ P4_ESP_CAM/esp_p4_SPI_master_display/main/example_config.h
@@ -0,0 +1,19 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+#include "sdkconfig.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define EXAMPLE_RGB565_BITS_PER_PIXEL           16
+
+#ifdef __cplusplus
+}
+#endif
